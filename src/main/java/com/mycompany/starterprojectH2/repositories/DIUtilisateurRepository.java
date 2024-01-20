5130599c5f6f254687bb0a1e1c7ea1f930e2ac38
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-01-21 00:50:17 +0100

    repo test

---
+++ src/main/java/com/mycompany/starterprojectH2/repositories/DIUtilisateurRepository.java
@@ -0,0 +1,13 @@
+package com.mycompany.starterprojectH2.repositories;
+
+import com.mycompany.starterprojectH2.entity.DI;
+import com.mycompany.starterprojectH2.entity.DIUtilisateur;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DIUtilisateurRepository extends CrudRepository<DIUtilisateur, Long> {
+
+DI di1 = new DI();
+
+
+
+}
---
+++ src/main/java/com/mycompany/starterprojectH2/repositories/DiRepository.java
@@ -0,0 +1,7 @@
+package com.mycompany.starterprojectH2.repositories;
+
+import com.mycompany.starterprojectH2.entity.DI;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DiRepository extends CrudRepository<DI, Long> {
+}
