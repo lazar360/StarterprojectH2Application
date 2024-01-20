3631374fbe90fb80902dd419f9b8754934169d4c
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-01-20 18:03:40 +0100

    starterproject spring avec H2

---
+++ src/main/java/com/mycompany/starterprojectH2/StarterprojectH2Application.java
@@ -0,0 +1,13 @@
+package com.mycompany.starterprojectH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterprojectH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterprojectH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mycompany/starterprojectH2/StarterprojectH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.starterprojectH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterprojectH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
