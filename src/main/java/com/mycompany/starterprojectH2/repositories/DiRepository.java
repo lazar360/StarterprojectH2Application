c4128874c4be474e4c38ff195b8cce4f9df14648
NicolasGautier <nicolas.gautier35135@gmail.com>
2024-01-21 14:29:58 +0100

    env de test

--- src/main/java/com/mycompany/starterprojectH2/repositories/DiRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mycompany.starterprojectH2.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface DiRepository extends CrudRepository<DI, Long> {
-}
